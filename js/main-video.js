adbf7d535b2ab9ed09fe0512d7db0da3ab0516db
lOtaviool <luisotavio95@gmail.com>
2026-04-07 17:35:09 -0300

    second commit

--- js/main-video.js
+++ js/main-video.js
@@ -1,7 +1,7 @@
 const section = document.getElementById("main-video");
 
 const iframe = document.createElement("iframe");
-iframe.src = "https://www.youtube.com/embed/MU8B4XDI3Uw";
+iframe.src = "https://www.youtube.com/embed/1wn-OSiNVjE";
 iframe.width = "960";
 iframe.height = "540";
 iframe.allow = "autoplay; encrypted-media";
